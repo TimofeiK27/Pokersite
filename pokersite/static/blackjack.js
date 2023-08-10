437ad93f59c33bb35198717c9437da952ce6cc8e
Timofei Kachan <67656268+TimofeiK27@users.noreply.github.com>
2023-08-10 15:49:51 -0700

    almost there

--- pokersite/static/blackjack.js
+++ pokersite/static/blackjack.js
@@ -4,7 +4,7 @@ decks = 1;
 
 hands = [];
 
-
+activeHand = 1;
 const hand = {
     count: 0,
     soft: false,
@@ -13,58 +13,78 @@ const hand = {
     doubleD:false,
     won:true,
     splitCards:[],
+    cards:1,
     hit() {
-        console.log(this);
+        this.cards++;
+        console.log(this.cards);
         document.getElementById('double-btn').hidden = 'true';
     
         newCard = document.createElement('div');
         newCard.className = 'card-slot';
     
-        newCardVal = selectCard();
+        //newCardVal = selectCard();
+        newCardVal = '04S'
         newCard.innerHTML = `<image class="cardimg" src="/static/media/${newCardVal}.jpg">`;
-    
+        console.log(newCardVal);
         newCardVal = parse10s(newCardVal);
-        
-            this.count += newCardVal;
-        
-            if(newCardVal == 1)
-                this.soft = true;
-        
-            if(this.count > 10)
-                this.soft = false;
-            if(this.count > 21 && this.handcount!=0){
-                this.won=false;
-                document.getElementById('functions').innerHTML = "BUST!";
-                document.getElementById('functions').innerHTML += '<button id="reset-btn">RESET</button>';
-                
-                fund();
-        
-                document.getElementById('reset-btn').addEventListener('click', reset);
-            }    
-     
-        
-        this.soft ? document.getElementById(`counter${this.handcount}`).innerHTML = `Soft ${this.count + 10}` : document.getElementById(`counter${this.handcount}`).innerHTML = `Hard ${this.count}`;   
-        console.log(this.handcount);
-        this.handcount == 0 ? document.getElementById('dealer-cards').append(newCard) : document.getElementById(`player-cards${this.handcount}`).append(newCard);
-        
+        newCard.id = `${this.handcount}${this.cards}`;
+
+        this.count += newCardVal;
     
+        if(newCardVal == 1)
+            this.soft = true;
+    
+        if(this.count > 11)
+            this.soft = false;
+
+        this.soft ? document.getElementById(`counter${this.handcount}`).innerHTML = `Soft ${this.count + 10}` : document.getElementById(`counter${this.handcount}`).innerHTML = `Hard ${this.count}`;   
+        this.handcount == 0 ? document.getElementById('dealer-cards').append(newCard) : document.getElementById(`player-cards${this.handcount}`).append(newCard);
+        ``
+        if(this.count > 21 && this.handcount!=0){
+            this.won=false;
+            if(this.handcount == hands.length -1){
+                wonHand = false;
+                for(i=1; i<hands.length-1; i++){
+                    if(hands[i].won)
+                        wonHand = true;
+                }
+                wonHand ? this.stand() : hands[0].soft ? fund(hands[0].count + 10) : fund(hands[0].count) ;
+
+                console.log(this);
+
+                document.getElementById('reset-btn').addEventListener('click', reset);
+
+            } else {
+                
+                document.getElementById(`player-cards${activeHand}`).style.border = '';
+                activeHand ++;
+                document.getElementById(`player-cards${activeHand}`).style.border = 'lightgray solid 2px';  
+                document.getElementById('double-btn').hidden = false; 
+            }
+            
+        } 
+        return newCardVal;
+
     },
 
     stand() {
+        console.log(333);
+        document.getElementById('double-btn').hidden = false; 
         if(this.soft){
             this.count+=10;
         }
 
-        if(this.handcount + 1 == hands.length){
-            document.getElementById('dealer-card2').remove();
-            dealerCards = document.getElementById('dealer-cards');
+        if(activeHand != hands.length-1){
+            document.getElementById(`player-cards${activeHand}`).style.border = '';
+            activeHand ++;
+            document.getElementById(`player-cards${activeHand}`).style.border = 'lightgray solid 2px';
+            return;
+        }
 
-
-            hands[0].hit();
-            console.log(33);
-        };
-
-           
+        
+        document.getElementById('dealer-card2').remove();
+        dealerCards = document.getElementById('dealer-cards');
+        
 
         while(testWin()) {
             hands[0].hit();
@@ -81,10 +101,17 @@ const hand = {
     },
     
     split(){
+        
+        document.getElementById(`${this.handcount}2`).remove();
         const hand2 = Object.create(hand);
 
         newHand = document.createElement('div');
         newHand.id = `player-cards${hands.length}`;
+        newHand.className = 'player-cards';
+        counter = document.createElement('h1');
+        counter.id = `counter${hands.length}`;
+        counter.style.display = 'block';
+        newHand.append(counter);
         document.getElementById('player-hands').append(newHand);
 
         newCard1 = document.createElement('div');
@@ -93,12 +120,27 @@ const hand = {
         newHand.append(newCard1);
     
 
-        console.log(hand2);
-        this.count = parse10s(this.splitCards[1]);
 
-        document.getElementById('player-card2').remove();
-        hand2.count = parse10s(this.splitCards[1]);
-        hands.push[hand2];
+        handVal = parse10s(this.splitCards[1]);
+        this.count = handVal;
+        hand2.count = handVal;
+
+        if(handVal == 1)
+            hand2.soft = true;
+        
+        hand2.handcount = hands.length;
+
+        hands.push(hand2);
+
+        new1 = hands[1].hit();
+        new2 = hands[2].hit();
+
+        document.getElementById(`player-cards${activeHand}`).style.border = 'lightgray solid 2px';
+
+        new1 == parse10s(this.splitCards[1]) ? console.log(new1) : document.getElementById(`split-btn`).remove();
+        
+        document.getElementById('double-btn').hidden = false;
+        this.cards--;
     }
 }
 
@@ -134,13 +176,12 @@ document.addEventListener('DOMContentLoaded', function() {
             
             document.getElementById('functions').innerHTML = `<button id="stand-btn"> STAND </button> <button id="hit-btn"> HIT </button> <button id="double-btn"> DOUBLE </button>`;
             setPlayerCards();
-            console.log(hands[0]);
-            document.getElementById('hit-btn').addEventListener('click', () => hands[1].hit());
-            document.getElementById('stand-btn').addEventListener('click', () => hands[1].stand());
+            document.getElementById('hit-btn').addEventListener('click', () => hands[activeHand].hit());
+            document.getElementById('stand-btn').addEventListener('click', () => hands[activeHand].stand());
 
             doublebtn = document.getElementById('double-btn');
 
-            bet * 2 <= funds ? doublebtn.addEventListener('click', () => hands[1].double()) :  doublebtn.style.background = 'darkgray';
+            bet * 2 <= funds ? doublebtn.addEventListener('click', () => hands[activeHand].double()) :  doublebtn.style.background = 'darkgray';
 
             document.getElementById('deal').hidden = true;
         }
@@ -152,7 +193,6 @@ function selectCard() {
     card = deck[Math.floor((deck.length-1) * Math.random())];
     index = deck.indexOf(card)
     deck.splice(index,1);
-    console.log(card);
     return card;
 }
 
@@ -167,8 +207,8 @@ function setPlayerCards() {
     card2 = document.createElement('div');
     card1.className = 'card-slot';
     card2.className = 'card-slot';
-    card1.id = 'player-card1';
-    card2.id = 'player-card2';
+    card1.id = '11';
+    card2.id = '12';
 
     dealerCards.innerHTML = '';
     dcard1 = document.createElement('div');
@@ -187,8 +227,8 @@ function setPlayerCards() {
     playerCards.append(card1);
     playerCards.append(card2);
 
-    playerCard1 = document.getElementById('player-card1');
-    playerCard2 = document.getElementById('player-card2');
+    playerCard1 = document.getElementById('11');
+    playerCard2 = document.getElementById('12');
 
     
     card1 = selectCard();
@@ -196,7 +236,7 @@ function setPlayerCards() {
     
     card1 = "04H";
     card2 = "04S";
-    //
+
     card1val = parse10s(card1);
     card2val = parse10s(card2);
 
@@ -224,6 +264,7 @@ function setPlayerCards() {
 
     hands.push(hand0);
     hands.push(hand1);
+    
     if(card1val == card2val){
         btn = document.createElement('button');
         btn.innerHTML = "- SPLIT -";
@@ -233,17 +274,11 @@ function setPlayerCards() {
         temp.push(card1);
         temp.push(card2);
         hand1.splitCards = temp;
-        console.log(hands[1]);
        document.getElementById("split-btn").addEventListener('click', ()=> hands[1].split());
         
     }
 
 }
-
-
-
-var dealerSoft;
-var dealerCount;
 
 
 function parse10s(card) {
@@ -252,36 +287,26 @@ function parse10s(card) {
 }
 
 function testWin(){
-    console.log(hands[0].count);
+
+   // hands[0].soft == true ? trueCount = (10 + hands[0].count) : trueCount =  hands[0].count;
+
     if(hands[0].count > 21){
-        fund();
-        document.getElementById('functions').innerHTML = "DEALER BUST!";
-        document.getElementById('functions').innerHTML += '<button id="reset-btn">RESET</button>';
-        document.getElementById('reset-btn').addEventListener('click', reset);
+        // -1 is passed when dealer busts
+        fund(-1);
         return false;
     }
     if((!hands[0].soft && hands[0].count >= 17) || (hands[0].soft && hands[0].count+10 >17)) {
-        if(hands[0].soft)
-        hands[0].count+=10;
-       if(hands[1].count > hands[0].count){
-            document.getElementById('functions').innerHTML = "Player Wins! ";
-            
-            fund();
-       } else if (hands[1].count < hands[0].count){
-            document.getElementById('functions').innerHTML = "Dealer Wins! ";
-            hands[1].won = false;
-            fund();
-       } else {
-            document.getElementById('functions').innerHTML = "Standoff ";
-       } ;
-       document.getElementById('functions').innerHTML += '<button id="reset-btn">RESET</button>';
-       document.getElementById('reset-btn').addEventListener('click', reset);
-       return false;
+        hands[0].soft ? fund(hands[0].count + 10) : fund(hands[0].count);
+        return false; 
     }
     return true;
 }
 
 function reset() {
+    for(i=2; i<hands.length; i++){
+        document.getElementById(`player-cards${i}`).remove();
+    }
+    activeHand = 1;
 
     temp = [];
     hands = temp;
@@ -296,30 +321,40 @@ function reset() {
             document.getElementById('reshuffle').innerHTML = "Reshuffled Deck!"
         } 
 
-        setPlayerCards();
-
-        
-
         document.getElementById('functions').innerHTML = `<button id="stand-btn"> STAND </button> <button id="hit-btn"> HIT </button> <button id="double-btn"> DOUBLE </button>`;
-        document.getElementById('hit-btn').addEventListener('click', ()=> hands[1].hit());
-        document.getElementById('stand-btn').addEventListener('click', ()=> hands[1].stand());
+
+        setPlayerCards();
+       
+
+        document.getElementById('hit-btn').addEventListener('click', ()=> hands[activeHand].hit());
+        document.getElementById('stand-btn').addEventListener('click', ()=> hands[activeHand].stand());
         doublebtn = document.getElementById('double-btn');
-        bet * 2 <= funds ? doublebtn.addEventListener('click', ()=> hands[1].double()) :  doublebtn.style.background = 'darkgray';
+        bet * 2 <= funds ? doublebtn.addEventListener('click', ()=> hands[activeHand].double()) :  doublebtn.style.background = 'darkgray';
 
         
     }
 }
 
-function fund() {
-    
+function fund(dealerCount) {
+    preFunds = funds;
     for(i=1; i<hands.length; i++){
-        console.log(hands[i]);
-        if(hands[i].won){
+        if(hands[i].won && hands[i].count > dealerCount){
             hands[i].doubleD ? funds += bet*2 : funds += bet;
-        } else {
+        } else if (hands[i].count != dealerCount){
             hands[i].doubleD ? funds -= bet*2 : funds -= bet;
         }
     }
+    console.log(funds - preFunds);
+    if (dealerCount == -1){
+        document.getElementById('functions').innerHTML = `DEALER BUST!`;
+
+    } else {
+        delta = funds - preFunds;
+        delta < 0 ? document.getElementById('functions').innerHTML = `Lost ${delta * -1} ` : document.getElementById('functions').innerHTML = `Won ${delta} `;
+    }
+
+    document.getElementById('functions').innerHTML += '<button id="reset-btn">RESET</button>';
+    document.getElementById('reset-btn').addEventListener('click', reset);    
     document.getElementById('fund-counter').innerHTML = `${funds} funds`;
 
 }
